f882ee239871d218fd195ca05ff9b4950f0f48f1
Imed-Ben-Fatma <imad.benfatma98@gmail.com>
2024-03-20 00:03:05 +0100

    Add Authentication and Authorization

--- src/main/java/com/example/garderieapi/Config/SecurityConfig.java
+++ src/main/java/com/example/garderieapi/Config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.garderieapi.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -35,15 +36,15 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf().disable()
-                .authorizeHttpRequests((authorize) ->
-                        //authorize.anyRequest().authenticated()
+        http.csrf( cerf -> cerf.disable() );
+        http.authorizeHttpRequests((authorize) ->
                         authorize.requestMatchers("/api/**").permitAll()
                                 .requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/upload/**").hasRole("ADMIN")
                                 .anyRequest().authenticated()
 
                 );
-
+        http.httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
--- src/main/java/com/example/garderieapi/Controller/FileController.java
+++ src/main/java/com/example/garderieapi/Controller/FileController.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 
 @RestController
-@RequestMapping("/api/upload")
+@RequestMapping("/upload")
 public class FileController {
 
     private final FileService fileService;
